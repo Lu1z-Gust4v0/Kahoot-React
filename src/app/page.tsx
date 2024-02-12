b8f4c10e548bce41e4c48fd5f3229c957d5cea25
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-02-11 22:46:21 -0300

    style: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,55 @@
 "use client";
-import { useState } from "react";
-import Slide from "@/components/Slide";
-import QuestionEditor from "@/components/QuestionEditor";
-import { Question } from "@/types/question";
+import Link, { LinkProps } from "next/link";
+import { ReactNode, useState } from "react";
+import Dialog from "@/components/Dialog";
 
-const defaultQuestion: Question = {
-  id: Math.random().toString(),
-  title: "",
-  body: "",
-  option_one: "",
-  option_two: "",
-  option_three: "",
-  option_four: "",
-  correct: [],
+type LinkButtonProps = {
+  children: ReactNode;
+  className?: string;
+} & LinkProps;
+
+const LinkButton = ({ children, className, ...props }: LinkButtonProps) => {
+  return (
+    <Link
+      className={`self-start py-4 px-16 rounded-md bg-blue-500 text-white text-2xl font-bold hover:bg-blue-700 ${className}`}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export default function Home() {
-  const [questions, setQuestions] = useState<Question[]>([defaultQuestion]);
-  const [selectedQuestion, setSelectedQuestion] =
-    useState<Question>(defaultQuestion);
+  const [open, setOpen] = useState(false);
 
-  const selectQuestion = (id: string) => {
-    questions.forEach((question) => {
-      if (question.id === id) setSelectedQuestion(question);
-    });
-  };
-
-  const addNewQuestion = () => {
-    setQuestions((previous) => [
-      ...previous,
-      {
-        id: Math.random().toString(),
-        title: "",
-        body: "",
-        option_one: "",
-        option_two: "",
-        option_three: "",
-        option_four: "",
-        correct: [],
-      },
-    ]);
-  };
-
-  const editQuestion = (questionToUpdate: Question) => {
-    setQuestions(
-      questions.map((question) => {
-        if (question.id === questionToUpdate.id) {
-          return questionToUpdate;
-        }
-        return question;
-      }),
-    );
+  const toggle = () => {
+    setOpen(!open);
   };
 
   return (
-    <main className="grid grid-cols-12 h-screen w-full bg-gray-200">
-      <Slide
-        questions={questions}
-        selectQuestion={selectQuestion}
-        addNewQuestion={addNewQuestion}
-      />
-      <QuestionEditor
-        selectedQuestion={selectedQuestion}
-        setSelectedQuestion={setSelectedQuestion}
-        editQuestion={editQuestion}
-      />
-    </main>
+    <div className="grid grid-cols-12 h-screen w-full bg-gray-200">
+      <nav className="flex items-center bg-white h-20 col-span-full px-8 shadow-md">
+        <h1 className="text-3xl font-bold text-gradient">Kahoot React</h1>
+      </nav>
+      <main className="min-h-[90vh] col-span-full md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8 flex flex-col py-8 px-2 overflow-y-auto">
+        {open && <Dialog close={toggle} />}
+        <div className="flex flex-col items-center gap-8 py-4 px-8 bg-white shadow-md rounded-md">
+          <h2 className="text-4xl text-gradient font-bold">
+            Select an option
+          </h2>
+          <div className="flex flex-col md:flex-row justify-center gap-8">
+            <LinkButton
+              href="/quiz/edit"
+              className="bg-purple-500 hover:bg-purple-700"
+            >
+              New Quiz
+            </LinkButton>
+            <LinkButton href="#" onClick={() => toggle()}>
+              Enter a Quiz
+            </LinkButton>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
