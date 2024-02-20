8807251009de9eaab3b99094df024dc61bab9960
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-02-20 16:23:00 -0300

    styles: add classname prop to toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -4,18 +4,24 @@ type ToastProps = {
   close: () => void;
   type: string;
   message: string;
+  className?: string;
 };
 
-const Toast = ({ message, type, close }: ToastProps) => {
+const Toast = ({ message, type, close, className }: ToastProps) => {
   return (
-    <div className="absolute justify-self-end flex flex-col h-fit min-w-80 w-1/4 m-8 bg-white py-4 px-4 rounded-md z-20 shadow-md">
+    <div
+      className={
+        "absolute justify-self-end flex flex-col h-fit min-h-48 min-w-80 w-1/4 m-8 bg-white py-4 px-4 rounded-md z-20 shadow-md " +
+        className
+      }
+    >
       <div className="flex justify-end">
         <button className="group self-start" onClick={() => close()}>
           <IoClose className="w-4 h-4 transition-colors duration-200 group-hover:text-blue-500 group-focus:text-blue-500" />
         </button>
       </div>
       <p
-        className={`text-lg ${type === "error" ? "text-red-500" : "text-green-700"
+        className={`text-xl ${type === "error" ? "text-red-500" : "text-green-700"
           }`}
       >
         {message}
