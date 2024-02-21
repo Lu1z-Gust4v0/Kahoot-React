9c0a2850bddf343a7e63eaeacd4998d884678dbd
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-02-21 11:02:13 -0300

    add: redirect to play game page from dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,10 +1,24 @@
+"use client"
+import { ChangeEvent, FormEvent, useState } from "react";
 import { IoClose } from "react-icons/io5";
+import { useRouter } from "next/navigation";
 
 type DialogProps = {
   close: () => void;
 };
 
 const Dialog = ({ close }: DialogProps) => {
+  const [code, setCode] = useState("")
+  
+  const router = useRouter()
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setCode(e.target.value)
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    router.push(`/quiz/play?code=${code}`)
+  }
+
   return (
     <div
       className="absolute flex justify-center inset-0 bg-black/50 py-32 z-10"
@@ -16,7 +30,10 @@ const Dialog = ({ close }: DialogProps) => {
         close();
       }}
     >
-      <div className="flex flex-col h-fit min-w-80 w-1/4 bg-white py-4 px-4 gap-4 rounded-md z-20">
+      <form 
+        className="flex flex-col h-fit min-w-80 w-1/4 bg-white py-4 px-4 gap-4 rounded-md z-20"
+        onSubmit={handleSubmit}
+      >
         <div className="flex mb-4 justify-between">
           <h3 className="text-4xl text-blue-500 font-bold">Quiz Code</h3>
           <button className="group self-start" onClick={() => close()}>
@@ -28,11 +45,13 @@ const Dialog = ({ close }: DialogProps) => {
           className="py-6 rounded-md px-4 shadow-md"
           type="text"
           placeholder="Quiz Code"
+          onChange={handleChange}
+          value={code}
         />
         <button className="py-4 px-16 rounded-md bg-blue-500 text-white text-2xl font-bold hover:bg-blue-700">
           Enter
         </button>
-      </div>
+      </form>
     </div>
   );
 };
